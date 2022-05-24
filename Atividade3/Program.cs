677b5cb571ede70c9d9ae16f1c346d662f09a919
Leonardo Belinato <70040882+haid3@users.noreply.github.com>
2022-05-24 00:43:18 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeA, nomeB;
+            Console.WriteLine("Informe o Nome:");
+            nomeA = Console.ReadLine();
+            Console.WriteLine("Informe o Ano do Nascimento de " + nomeA + ":");
+            int anoNascA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o outro Nome:");
+            nomeB = Console.ReadLine();
+            Console.WriteLine("Informe o Ano do Nascimento de " + nomeB + ":");
+            int anoNascB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o Ano Atual: ");
+            int anoAtual = int.Parse(Console.ReadLine());
+            int idadeA, idadeB, difIdade;
+            idadeA = anoAtual - anoNascA;
+            idadeB = anoAtual - anoNascB;
+            if (idadeA > idadeB)
+            {
+                difIdade = idadeA - idadeB;
+            }
+            else
+            {
+                difIdade = idadeB - idadeA;
+
+            }
+            Console.WriteLine("A idade de: " + nomeA + " é: " + idadeA);
+            Console.WriteLine("A idade de: " + nomeB + " é: " + idadeB);
+            Console.WriteLine("A diferença de idade entre os dois é: " + difIdade + " anos");
+            Console.Read();
+        }
+    }
+}
